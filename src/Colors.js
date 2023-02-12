bd6e20a3286b9dd8ba9f5f8ff26537c11e3950ef
Yadnik <yadnik.chavan@wondrx.info>
2023-02-13 01:14:38 +0530

    First Commit

---
+++ src/Colors.js
@@ -0,0 +1,17 @@
+const Colors = {
+  main: "#48B600",
+  white: "#FFFFFF",
+  black: "#000000",
+  lightBlack: "#BCBCBC",
+  orange: "#FFCE31",
+  red: "a#FF0000",
+  gray: "#F4F4F4",
+  subGreen: "#EEFAE6",
+  deepGray: "#E1FOD7",
+  deepestGray: "#C4C4C4",
+  blue: "#3B87F9",
+  underline: "#ESESE5",
+  paypal: "#FFB730",
+};
+
+export default Colors;
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'native-base'
+
+function LoginScreen() {
+  return (
+    <View>
+        <Text>LoginScreen</Text>
+    </View>
+  )
+}
+
+export default LoginScreen
